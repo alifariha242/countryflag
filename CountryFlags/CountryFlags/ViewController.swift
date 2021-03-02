d0b2b6e9c65948894d4d87c18dd45c5f13ec839e
Fariha Mohamed <alifariha242@gmail.com>
2021-03-02 13:59:20 -0800

    tableview implementation

--- CountryFlags/CountryFlags/ViewController.swift
+++ CountryFlags/CountryFlags/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countryflagtableview: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+}
 
-
+extension ViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tablecellidentifier", for: indexPath)
+        return cell
+    }
 }
 
