77b584486ee0462607892fba90fc616887c2fbc6
Fariha Mohamed <alifariha242@gmail.com>
2021-03-02 12:40:43 -0800

    first commit

---
+++ CountryFlags/CountryFlags/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CountryFlags
+//
+//  Created by Fariha Mohamed on 3/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
